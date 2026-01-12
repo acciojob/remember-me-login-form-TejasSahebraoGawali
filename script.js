f9bd82ae7082c98e1d83b2080203340d7e2eead1
Tejas Sahebrao Gawali <156706283+TejasSahebraoGawali@users.noreply.github.com>
2026-01-12 23:41:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+let submitBtn = document.querySelector("#submit");
+let loginBtn = document.querySelector("#existing");
+let username = document.querySelector("#username");
+let password = document.querySelector("#password");
+let rememberMe = document.querySelector("#checkbox");
+
+rememberMe.addEventListener("change", (e) => {
+	if(e.target.checked) {
+		localStorage.setItem("username", username);
+		localStorage.setItem("password", password);
+	} else if(!e.target.checked) {
+		localStorage.removeItem("username");
+		localStorage.removeItem("password");
+	}
+
+	const storedUsername = localStorage.getItem("username");
+
+    if (storedUsername !== null) {
+        loginBtn.style.display = "block";
+    } else {
+        
+    }
+})
+
+submitBtn.addEventListener("click", () => {
+	alert(`Logged in as ${username.value}`);
+})
+
+loginBtn.addEventListener("click", () => {
+	alert("Logged in as <saved username>");
+})
